6748b2879b64ed9550d078e3a5c0b9f5fd5c1f11
darewis <devinrewis@gmail.com>
2017-02-06 21:13:18 -0800

    Testing better function for reading from S3

--- json_to_orc.py
+++ json_to_orc.py
@@ -12,7 +12,8 @@
 from pyspark import SparkContext
 from pyspark import StorageLevel
 from pyspark.sql import SparkSession
-from boto.s3.connection import S3Connection
+from pyspark.sql import Row
+import boto3
 import json
 import yaml
 
@@ -28,7 +29,7 @@ with open("settings.yaml", 'r') as stream:
         settings = yaml.load(stream)
     except yaml.YAMLError as exc:
         print(exc)
-
+'''
 def fetch_files(key):
     for line in key.get_contents_as_string().splitlines():
         j = json.loads(line)
@@ -38,6 +39,19 @@ conn = S3Connection()
 bucket = conn.get_bucket(settings['json-data'])
 keys = sc.parallelize(bucket.list())
 files = keys.flatMap(fetch_files)
+'''
+s3 = boto3.resource('s3')
+bucket = s3.Bucket(settings['json-data'])
+key_list = [k.key for k in bucket.objects.all()]
+
+def distributedJsonRead(s3Key):
+    s3obj = boto3.resource('s3').Object(bucket_name=settings['json-data'], key=s3Key)
+    contents = json.loads(s3obj.get()['Body'].read().decode('utf-8'))
+    for dicts in content['interactions']
+        yield Row(**dicts)
+
+pkeys = sc.parallelize(keyList) #keyList is a list of s3 keys
+dataRdd = pkeys.flatMap(distributedJsonRead)
 
 print(files.top(3))
 #file = sc.textFile(settings['json-data']).persist(StorageLevel(True, True, False, False, 1))
