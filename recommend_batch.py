3448bb725d8679c3715d08da4d717ec6a26e3880
darewis <devinrewis@gmail.com>
2017-02-08 16:52:57 -0800

    Testing cartesian product

--- recommend_batch.py
+++ recommend_batch.py
@@ -113,8 +113,6 @@ def cosineSim(aVectors, bVectors):
     #similar = self.vectorSpace.rdd.mapValues(lambda b: (a.dot(b))/(a_mag * b.norm(2))) \
     #    .sortBy(lambda x: x[1], ascending=False) #sort values for output
     
-    print(vectors.rdd.collect())
-    
     results = vectors.rdd.map(lambda row: [row.a, row.b, 
                                           (row.a_vector.dot(row.b_vector))/
                                           (row.a_vector.norm(2) * row.b_vector.norm(2))]
@@ -128,8 +126,8 @@ def cosineSim(aVectors, bVectors):
 subreddit_vectors = sqlContext.read.parquet(settings['subreddit-vectors'])
 author_vectors = sqlContext.read.parquet(settings['author-vectors'])
 
-subreddit_vectors = subreddit_vectors.limit(3)
-author_vectors = author_vectors.limit(3)
+subreddit_vectors = subreddit_vectors.limit(10)
+author_vectors = author_vectors.limit(10)
 
 result = cosineSim(author_vectors, subreddit_vectors)
 print(result.collect())
