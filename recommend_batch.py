e669ce17e9f122a688e027fc3a208793d9b22ba2
darewis <devinrewis@gmail.com>
2017-02-08 19:30:14 -0800

    Optimizing output to Redis

--- recommend_batch.py
+++ recommend_batch.py
@@ -133,6 +133,10 @@ author_vectors = author_vectors.limit(10)
 result = cosineSim(author_vectors, subreddit_vectors)
 result = result.reduceByKey(add)
 
+def f(x):
+    rdb.hset('authortest', x[0].lower(), json.dumps(x[1])))
+result.map(f)
+
 out = result.map(lambda x: [x[0].lower(), json.dumps(x[1])])
 print(out.collect())
 
