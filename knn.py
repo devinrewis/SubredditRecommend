8c81ff39d6ba0e3e524ef664f68efb093c298d98
darewis <devinrewis@gmail.com>
2017-02-11 20:34:35 -0800

    Formatting output for Redis

--- knn.py
+++ knn.py
@@ -59,7 +59,8 @@ a_results = author_vectors_df.rdd.map(lambda x: [x.author, lshf.kneighbors(x.vec
 s_results = s_results.map(lambda x: [x[0], x[1][0].tolist()[0], x[1][1][0].tolist()])
 a_results = a_results.map(lambda x: [x[0], x[1][0].tolist()[0], x[1][1][0].tolist()])
 
-s_results = s_results.map(lambda x: [x[0], [[local_sub_names[k], x[1][k]] for k in x[2]]])
+
+s_results = s_results.map(lambda x: [x[0], [[local_sub_names[x[2][k]], x[1][k]] for k in x[2]]])
 
 
 print(s_results.take(10))
