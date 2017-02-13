cb4ae3aa11fffd78bb4d3e5ad038d06f9f98aa23
darewis <devinrewis@gmail.com>
2017-02-12 17:29:38 -0800

    Fixed bug where recommendations were not converted to JSON before being inserted into Redis

--- distribute_redis.py
+++ distribute_redis.py
@@ -1,5 +1,6 @@
 import redis
 import yaml
+import json
 
 with open("settings.yaml", 'r') as stream:
     try:
@@ -10,7 +11,7 @@ with open("settings.yaml", 'r') as stream:
 rdb = redis.StrictRedis(host=settings['redis-host'], port=6379, db=0)
 
 def deliver_author_redis(x):
-    rdb.hset('authorknn', x[0].lower(), x[1])
+    rdb.hset('authorknn', x[0].lower(), json.dumps(x[1]))
 
 def deliver_sub_redis(x):
-    rdb.hset('subredditknn', x[0].lower(), x[1])
+    rdb.hset('subredditknn', x[0].lower(), json.dumps(x[1]))
